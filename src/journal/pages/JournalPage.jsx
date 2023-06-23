3f012ecdc37263cba9f81602bb47e12e44f4b1b4
julian-gallo-nexer <julian.gallo2@nexergroup.com>
2023-06-23 15:55:10 -0500

    nothing selected view created

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,6 +1,7 @@
 import { MailOutline } from "@mui/icons-material"
 import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NothingSelectedView } from "../views";
 
 const drawerWidth = 240;
 
@@ -9,9 +10,7 @@ export const JournalPage = () => {
     <>
 
       <JournalLayout>
-        <Typography>
-          ricies tristique nulla aliquet enim tortor. Nullam eget felis eget nunc lobortis mattis aliquam faucibus purus. Dui id ornare arcu odio ut sem. Mi bibendum neque egestas congue quisque egestas diam in arcu. Ultrices vitae auctor eu augue ut. Enim ut tellus elementum sagittis vitae et leo duis ut. Proin fermentum leo vel
-        </Typography>
+        <NothingSelectedView />
       </JournalLayout>
     </>
   )
