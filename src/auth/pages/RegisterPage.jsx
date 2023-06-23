7f9649e068f73be12da604eb30ab8e17e7708c45
julian-gallo-nexer <julian.gallo2@nexergroup.com>
2023-06-23 15:26:03 -0500

    basic structure

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,5 +1,48 @@
+import { Google } from "@mui/icons-material"
+import { Link as RouterLink } from 'react-router-dom'
+
+import { Button, Grid, Link, TextField, Typography } from "@mui/material"
+import { AuthLayout } from "../layout/AuthLayout"
+
 export const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <>
+      <AuthLayout title="Crear cuenta">
+        <form>
+          <Grid container>
+            <Grid item xs={12} sx={{ mt: 2 }}>
+              <TextField label="nombre" type="text" placeholder="Nombre" fullWidth />
+            </Grid>
+            <Grid item xs={12} sx={{ mt: 2 }}>
+              <TextField label="correo" type="email" placeholder="correo@test.com" fullWidth />
+            </Grid>
+            <Grid item xs={12} sx={{ mt: 2 }}>
+              <TextField label="contrasena" type="password" fullWidth />
+            </Grid>
+          </Grid>
+          <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+            <Grid item xs={12} sm={6}>
+              <Button variant='contained' fullWidth>
+                Crear cuenta
+              </Button>
+            </Grid>
+            {/* <Grid item xs={12} sm={6}>
+              <Button variant='contained' fullWidth>
+                <Google />
+                <Typography sx={{ ml: 1 }} >Google</Typography>
+              </Button>
+            </Grid> */}
+            <Grid container direction='row' justifyContent='end'>
+              <Typography sx={{ mr: 1 }}>
+                Ya tienes una cuenta?
+              </Typography>
+              <Link color='inherit' to="/auth/login" component={RouterLink}>
+                ingresar
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </AuthLayout>
+    </>
   )
 }
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,13 +1,18 @@
 import { MailOutline } from "@mui/icons-material"
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
+
+const drawerWidth = 240;
 
 export const JournalPage = () => {
   return (
     <>
-      <Typography variant="h1">
-        Journal page
-      </Typography>
-      <MailOutline />
+
+      <JournalLayout>
+        <Typography>
+          ricies tristique nulla aliquet enim tortor. Nullam eget felis eget nunc lobortis mattis aliquam faucibus purus. Dui id ornare arcu odio ut sem. Mi bibendum neque egestas congue quisque egestas diam in arcu. Ultrices vitae auctor eu augue ut. Enim ut tellus elementum sagittis vitae et leo duis ut. Proin fermentum leo vel
+        </Typography>
+      </JournalLayout>
     </>
   )
 }
